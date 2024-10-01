fd40945d08ad2f47e8cf25b40644f5e057e86941
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-10-01 18:25:11 -0300

    .

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  const [quote, setQuote] = useState("")
+  const [quoteAuthor, setQuoteAuthor] = useState("")
+  const [authorImage, setAuthorImage] = useState("")
+
+  function adquirir () {
+    const quotes = require('./quotes.json');
+    let random = Math.floor(Math.random() * quotes.length)
+    let randomQuote = quotes[random]
+    setQuote(randomQuote.frase)
+    setQuoteAuthor(randomQuote.autor)
+    setAuthorImage(randomQuote.imagem)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >{quote.length == 0 ? "Adquira sua frase motivacional" : ""} </Text>
+      <Button title={quote.length == 0 ? "Adquirir" : "Adquirir novamente"} onPress={adquirir} />
+
+
+      <Text style={styles.quote}>"{quote}"</Text>
+      <Text  style={styles.author}>{quoteAuthor}</Text>
+      <Image source={{ uri: authorImage }} style={styles.imagem}></Image>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  imagem : {
+    width: 100,
+    height: 100,
+    borderRadius: 100
+  },
+
+  quote: {
+    fontSize: 28,
+    fontWeight: 'semibold'
+  },
+
+  author: {
+    color: 'gray'
+  }
 });
